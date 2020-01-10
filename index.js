2ebbf2ca1988a02eb61193b4c8e64c3d6dd16175
saki <16546851+sakiko-2@users.noreply.github.com>
2020-01-11 01:09:05 +1300

    add contact, get contacts, get contact by ID, update contact, delete contact

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 4000;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/CRMdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
 
 app.get('/', (req, res) => (
   res.send(`Express Server running on port ${PORT}`)
